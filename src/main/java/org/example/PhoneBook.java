870ff5e213f381aab17f88c4207e0547f6ecfb20
pavelkurtukov <pavel@kurtukov.ru>
2023-09-20 15:09:55 +0700

    Добавлена заглушка метода add и тесты для него

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,17 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+
+    // Добавление имени и номера телефона
+    public int add(String name, long phone) {
+        return 0;
+    }
+
+    // Поиск телефона по имени
+    public long findByName(String name) {
+        return 0;
+    }
 }
--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -1,7 +1,32 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PhoneBookTest {
+    @Test
+    @DisplayName("Тест вставки и возврата количества записей")
+    void addCount() {
+        PhoneBook phoneBook = new PhoneBook();
+        int actualValue;
+        phoneBook.add("Иванов Иван", 79998887766l);
+        actualValue = phoneBook.add("Петров Пётр", 79998887755l);
 
+        Assertions.assertEquals(2, actualValue);
+    }
+
+    @Test
+    @DisplayName("Тест запрета добавлять повторяющиеся имена")
+    void addDuplicate() {
+        PhoneBook phoneBook = new PhoneBook();
+        int actualValue;
+        phoneBook.add("Иванов Иван", 79998887766l);
+        actualValue = phoneBook.add("Иванов Иван", 79998887755l);
+
+        Assertions.assertEquals(1, actualValue);
+        Assertions.assertEquals(79998887766l, phoneBook.findByName("Иванов Иван"));
+    }
 }
